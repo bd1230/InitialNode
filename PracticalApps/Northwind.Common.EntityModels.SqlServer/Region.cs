0b36ca8ea707e3c97e18b4a779f6e53c39e33cde
bd1230 <thebow@live.com>
2024-07-04 17:34:54 -0500

    Northwind practice application solution

---
+++ PracticalApps/Northwind.Common.EntityModels.SqlServer/Region.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Northwind.Shared
+{
+    [Table("Region")]
+    public partial class Region
+    {
+        public Region()
+        {
+            Territories = new HashSet<Territory>();
+        }
+
+        [Key]
+        [Column("RegionID")]
+        public int RegionId { get; set; }
+        [StringLength(50)]
+        public string RegionDescription { get; set; } = null!;
+
+        [InverseProperty("Region")]
+        public virtual ICollection<Territory> Territories { get; set; }
+    }
+}
---
+++ PracticalApps/Northwind.OData/Usings/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.AspNetCore.OData; // AddOData extension method
+global using Microsoft.OData.Edm; // IEdmModel
+global using Microsoft.OData.ModelBuilder; // ODataConventionModelBuilder
+global using Northwind.Shared; // NorthwindContext and entity models
+
+global using Microsoft.Extensions.Logging; //Microsoft Logging Extension
