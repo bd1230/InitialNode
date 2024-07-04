b3c333fdf0d2fe50f31c9438f884d79fe5d0e80f
bd1230 <thebow@live.com>
2024-07-04 17:36:07 -0500

    Delete SimTicker-Personal directory

--- SimTicker-Personal/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SimTicker_Personal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
